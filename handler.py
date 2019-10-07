9739e73a13221d847f12f2471d379eda088619aa
Gagan Chohan <gaganchohan@icloud.com>
2019-10-07 13:51:53 +1100

    First json response

--- handler.py
+++ handler.py
@@ -8,3 +8,13 @@ def hello(event, context):
 def createPost(event, context):
     print ("create post was called")
     return 1
+
+def getuserinfo(event, context):
+    print ("context is", context)
+    # print ("id is "+ id)
+    message = "This is the message"
+    return {
+        'statusCode' : 200,
+        'headers': {'Content-Type': 'application/json'},
+        'body': json.dumps({'message' : message })
+    }
